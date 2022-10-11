f5fe7f21730f2226013e6d3796b76b5e9246898a
GimungbiN <111366347+GimungbiN@users.noreply.github.com>
2022-10-11 13:57:37 +0900

    Update ForEx02.py

--- ForEx02.py
+++ ForEx02.py
@@ -1,6 +1,6 @@
 i, j = 0, 0
 
 for i in range(2, 10, 1) :
-    for j in range(1,10,1):
-        print("%d x %d"%i, j, i*j)
+    for j in range(2,10,1):
+        print("%2d x %2d = %2d"%(i, j, i*j),end="")
     print("")
